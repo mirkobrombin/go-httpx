440c842639c963a0192e787c2e275042eaa7fb75
Mirko Brombin <brombin94@gmail.com>
2026-04-26 17:01:16 +0200

    feat: add Logging and RequestID middleware

--- pkg/httpx/middleware.go
+++ pkg/httpx/middleware.go
@@ -1,17 +1,10 @@
 package httpx
 
-import "net/http"
-
-// Header adds a header to all outgoing requests.
-func Header(key, value string) Middleware {
-	return func(next http.RoundTripper) http.RoundTripper {
-		return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
-			cloned := cloneRequest(r)
-			cloned.Header.Set(key, value)
-			return next.RoundTrip(cloned)
-		})
-	}
-}
+import (
+	"log/slog"
+	"net/http"
+	"time"
+)
 
 type roundTripperFunc func(*http.Request) (*http.Response, error)
 
@@ -26,4 +19,37 @@ func cloneRequest(r *http.Request) *http.Request {
 		cloned.Header[k] = append([]string(nil), v...)
 	}
 	return cloned
+}
+
+func Logging(logger *slog.Logger) Middleware {
+	return func(next http.RoundTripper) http.RoundTripper {
+		return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+			start := time.Now()
+			resp, err := next.RoundTrip(r)
+			dur := time.Since(start)
+			if err != nil {
+				logger.Error("http request failed", "method", r.Method, "url", r.URL.String(), "duration", dur, "error", err)
+			} else {
+				logger.Info("http request", "method", r.Method, "url", r.URL.String(), "status", resp.StatusCode, "duration", dur)
+			}
+			return resp, err
+		})
+	}
+}
+
+func RequestID(header string) Middleware {
+	return func(next http.RoundTripper) http.RoundTripper {
+		return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+			if r.Header.Get(header) == "" {
+				cloned := cloneRequest(r)
+				cloned.Header.Set(header, newRequestID())
+				return next.RoundTrip(cloned)
+			}
+			return next.RoundTrip(r)
+		})
+	}
+}
+
+func newRequestID() string {
+	return time.Now().Format("20060102150405.000000")
 }
